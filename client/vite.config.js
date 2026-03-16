ce81d01275a38bf861467ce1605307856c590b3d
neovicon <240396sarun@cosmoscollege.edu.np>
2026-03-16 21:14:24 +0545

    Added socialbridge domain to vite allowlist

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['socialbridge.devsandbox.me'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
